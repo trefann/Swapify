7630d576e7412846b8404c4d7a9324244604d35c
trefan <Trefankagoo2016@gmail.com>
2025-10-20 06:36:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5993346001-f5e89",
+  "appId": "1:527206190785:web:697acbe91e13ce6d70ce40",
+  "apiKey": "AIzaSyBowFaWwcZjPtIolSLqVmFJS9NkdJsG3d0",
+  "authDomain": "studio-5993346001-f5e89.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "527206190785"
+};
